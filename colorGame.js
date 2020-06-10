264fc8c9fd33fbd0815eba59c05c26e0b45cafa7
Casey D <caseydasilva@hotmail.com>
2020-06-10 12:13:30 -0700

    initial randomization of colorPicker
    Added a randomization (Math.random) to select one of the colors from the colors[array] and it will assign that randomly selected color to the 'correct' color

--- colorGame.js
+++ colorGame.js
@@ -7,7 +7,7 @@ var colors = [
     "rgb(255, 0, 255)"
 ]
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 
@@ -24,6 +24,7 @@ for(var i = 0; i < squares.length; i++) {
         //compare color to pickedColor
         if(clickedColor === pickedColor){
             messageDisplay.textContent = "Correct";
+            changeColors(clickedColor);
         } 
             else{
             this.style.backgroundColor = "#232323";
@@ -32,3 +33,15 @@ for(var i = 0; i < squares.length; i++) {
     });
 }
 
+function changeColors(color) {
+    //loop through all squares
+    for(var i=0; i < squares.length; i++){
+        //change each color to match given color
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor(){
+   var random = Math.floor(Math.random() * colors.length)
+   return colors[random];
+}
